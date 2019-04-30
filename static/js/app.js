917e9dba5678cd8b1b5822ffb37d9c9f89b7f05f
Bruno Marques <bmarques8989@gmail.com>
2019-04-30 19:33:03 -0400

    completed

--- static/js/app.js
+++ static/js/app.js
@@ -1,34 +1,44 @@
 // from data.js
 let tableData = data;
-// console.log(tableData)
+console.log(tableData)
 
 const tbody = d3.select("tbody");
 
-tableData.forEach((x) => {
-	// console.log(x)
-	var row = tbody.append("tr");
-	Object.entries(x).forEach(([key, value]) => {
-    var cell = row.append("td");
-    cell.text(value);
-  });
+function addRows(newRows){
+	tbody.html("");
+
+	newRows.forEach((tableData) => {
+		var row = tbody.append("tr");
+		Object.entries(tableData).forEach(([key, value]) => {
+    	var cell = row.append("td");
+    	cell.text(value);
+  		});
+	});
+};
+
+addRows(tableData)
+//onClick Function
+
+var submit = d3.select("#filter-btn");
+
+submit.on("click", function() {
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#date");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  console.log("This is the inputvalue" + inputValue);
+
+  var filteredData = tableData.filter(table => table.datetime === inputValue);
+
+  console.log("This is filtereddata" + filteredData);
+
+  addRows(filteredData)
+
 });
 
-
-let dateInput = d3.select("#filter-btn")
-
-dateInput.on("click", function() {
-
-	d3.event.preventDefault();
-
-	let inputField = d3.select("#date").property("value")
-	
-
-	let filterDate = tableData.filter(x => x.datetime === inputField)
-
-	if filterDate === true{
-		let rows = table.select("tbody").selectAll("tr")
-		rows.sort
-	}
-
-
-});
